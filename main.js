3715afa17ac8c7701fbc9cea4fd27f570ae73e08
Tori Hedden <tori.hedden@gmail.com>
2018-08-18 11:37:30 -0400

    probably dont need to comit built files to non ghpages branch

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = ".action-icon {\r\n  fill: lightgray;\r\n  width: 15px;\r\n  margin-right: 15px;\r\n}\r\n\r\n.action-icon:hover {\r\n  fill: black;\r\n}\r\n"
+module.exports = "* {\r\n  font-family: sans-serif;\r\n}\r\n\r\ntd {\r\n  padding: 3px;\r\n}\r\n\r\n.action-icon {\r\n  fill: lightgray;\r\n  width: 15px;\r\n  margin-right: 15px;\r\n}\r\n\r\n.action-icon:hover {\r\n  fill: black;\r\n}\r\n"
 
 /***/ }),
 
@@ -41,7 +41,7 @@ module.exports = ".action-icon {\r\n  fill: lightgray;\r\n  width: 15px;\r\n  ma
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div>\r\n  <h1>{{ title }}</h1>\r\n\r\n  <div class=\"\">\r\n    <input [(ngModel)]=\"searchParam\" type=\"text\" name=\"\" value=\"\" placeholder=\"search\">\r\n    <button (click)=\"onSearch()\"\r\n            type=\"button\"\r\n            name=\"button\">Search\r\n    </button>\r\n  </div>\r\n  <br>\r\n\r\n  <div *ngIf=\"submittedSearch && !appointments.length && !errorMessage\"><h3>Loading...</h3></div>\r\n  <div *ngIf=\"errorMessage\"><h3>{{errorMessage}}</h3></div>\r\n\r\n  <table *ngIf=\"submittedSearch && appointments.length\">\r\n    <tr>\r\n      <th>Date</th>\r\n      <th>Time</th>\r\n      <th>Description</th>\r\n      <th>Actions</th>\r\n    </tr>\r\n    <tr *ngFor=\"let appointment of appointments\">\r\n      <th>{{appointment.dateTime | date: 'mediumDate'}}</th>\r\n      <th>{{appointment.dateTime | date: 'shortTime'}}</th>\r\n      <th>{{appointment.description}}</th>\r\n      <th>\r\n        <span (click)=\"deleteAppointment(appointment._id)\" title=\"Delete\">\r\n          <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 448 512\" class=\"action-icon\">\r\n            <path d=\"M0 84V56c0-13.3 10.7-24 24-24h112l9.4-18.7c4-8.2 12.3-13.3 21.4-13.3h114.3c9.1 0 17.4 5.1 21.5 13.3L312 32h112c13.3 0 24 10.7 24 24v28c0 6.6-5.4 12-12 12H12C5.4 96 0 90.6 0 84zm416 56v324c0 26.5-21.5 48-48 48H80c-26.5 0-48-21.5-48-48V140c0-6.6 5.4-12 12-12h360c6.6 0 12 5.4 12 12zm-272 68c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208z\"/>\r\n          </svg>\r\n        </span>\r\n        <span title=\"Edit\">\r\n          <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 576 512\" class=\"action-icon\">\r\n            <path d=\"M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z\"/>\r\n          </svg>\r\n        </span>\r\n      </th>\r\n    </tr>\r\n  </table>\r\n\r\n  <button (click)=\"toggleForm(true)\" type=\"button\" name=\"button\">New appointment</button>\r\n  <form *ngIf=\"createFormOpen\" class=\"\" action=\"index.html\" method=\"post\">\r\n    <label for=\"\">Date</label>\r\n    <input type=\"date\" name=\"date\" value=\"\" [(ngModel)]=\"formData.date\" required>\r\n    <br>\r\n    <label for=\"\">Time</label>\r\n    <input type=\"time\" name=\"time\" value=\"\" [(ngModel)]=\"formData.time\" required>\r\n    <br>\r\n    <label for=\"\">Description</label>\r\n    <input type=\"text\" name=\"description\" value=\"\" [(ngModel)]=\"formData.description\" required>\r\n    <br>\r\n    <button (click)=\"createAppointment(formData)\" type=\"button\" name=\"button\">Create</button>\r\n    <button (click)=\"cancelCreate(formData)\" type=\"button\" name=\"button\">Cancel</button>\r\n  </form>\r\n</div>\r\n\r\n<div style=\"position: fixed; left: 0; bottom: 0; width: 100%; text-align: center;\">\r\n  <p>View this project's source code <a href=\"https://github.com/torihedden/apptBack\" target=\"_blank\">here</a> and <a href=\"https://github.com/torihedden/appointment-app\" target=\"_blank\">here</a>.</p>\r\n</div>\r\n"
+module.exports = "<div>\r\n  <h1>{{ title }}</h1>\r\n\r\n  <div class=\"\">\r\n    <input [(ngModel)]=\"searchParam\" type=\"text\" name=\"\" value=\"\" placeholder=\"search\">\r\n    <button (click)=\"onSearch()\"\r\n            type=\"button\"\r\n            name=\"button\">Search\r\n    </button>\r\n  </div>\r\n  <br>\r\n\r\n  <div *ngIf=\"submittedSearch && !appointments.length && !errorMessage\"><h3>Loading...</h3></div>\r\n  <div *ngIf=\"errorMessage\"><h3>{{errorMessage}}</h3></div>\r\n\r\n  <button (click)=\"toggleForm(true)\" type=\"button\" name=\"button\">New appointment</button>\r\n  <form *ngIf=\"createFormOpen\" class=\"\" action=\"index.html\" method=\"post\">\r\n    <label for=\"\">Date</label>\r\n    <input type=\"date\" name=\"date\" value=\"\" [(ngModel)]=\"formData.date\" required>\r\n    <br>\r\n    <label for=\"\">Time</label>\r\n    <input type=\"time\" name=\"time\" value=\"\" [(ngModel)]=\"formData.time\" required>\r\n    <br>\r\n    <label for=\"\">Description</label>\r\n    <input type=\"text\" name=\"description\" value=\"\" [(ngModel)]=\"formData.description\" required>\r\n    <br>\r\n    <button (click)=\"createAppointment(formData)\" type=\"button\" name=\"button\">Create</button>\r\n    <button (click)=\"cancelCreate(formData)\" type=\"button\" name=\"button\">Cancel</button>\r\n  </form>\r\n\r\n  <table *ngIf=\"submittedSearch && appointments.length\">\r\n    <tr>\r\n      <th>Date</th>\r\n      <th>Time</th>\r\n      <th>Description</th>\r\n      <th>Actions</th>\r\n    </tr>\r\n    <tr *ngFor=\"let appointment of appointments\">\r\n      <td>{{ appointment.dateTime | date: 'mediumDate' }}</td>\r\n      <td>{{ appointment.dateTime | date: 'shortTime' }}</td>\r\n      <td>{{ appointment.description | upper }}</td>\r\n      <td>\r\n        <span (click)=\"deleteAppointment(appointment._id)\" title=\"Delete\">\r\n          <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 448 512\" class=\"action-icon\">\r\n            <path d=\"M0 84V56c0-13.3 10.7-24 24-24h112l9.4-18.7c4-8.2 12.3-13.3 21.4-13.3h114.3c9.1 0 17.4 5.1 21.5 13.3L312 32h112c13.3 0 24 10.7 24 24v28c0 6.6-5.4 12-12 12H12C5.4 96 0 90.6 0 84zm416 56v324c0 26.5-21.5 48-48 48H80c-26.5 0-48-21.5-48-48V140c0-6.6 5.4-12 12-12h360c6.6 0 12 5.4 12 12zm-272 68c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208z\"/>\r\n          </svg>\r\n        </span>\r\n        <span title=\"Edit\">\r\n          <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 576 512\" class=\"action-icon\">\r\n            <path d=\"M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z\"/>\r\n          </svg>\r\n        </span>\r\n      </td>\r\n    </tr>\r\n  </table>\r\n\r\n</div>\r\n\r\n<div style=\"position: fixed; left: 0; bottom: 0; width: 100%; text-align: center;\">\r\n  <p>View this project's source code <a href=\"https://github.com/torihedden/apptBack\" target=\"_blank\">here</a> and <a href=\"https://github.com/torihedden/appointment-app\" target=\"_blank\">here</a>.</p>\r\n</div>\r\n"
 
 /***/ }),
 
@@ -153,7 +153,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
 /* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
-/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+/* harmony import */ var _app_pipe__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./app.pipe */ "./src/app/app.pipe.ts");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -165,13 +166,15 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 
 
 
+
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["NgModule"])({
             declarations: [
-                _app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"]
+                _app_component__WEBPACK_IMPORTED_MODULE_5__["AppComponent"],
+                _app_pipe__WEBPACK_IMPORTED_MODULE_4__["UpperPipe"]
             ],
             imports: [
                 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
@@ -179,10 +182,46 @@ var AppModule = /** @class */ (function () {
                 _angular_common_http__WEBPACK_IMPORTED_MODULE_3__["HttpClientModule"]
             ],
             providers: [],
-            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"]]
+            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_5__["AppComponent"]]
         })
     ], AppModule);
     return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.pipe.ts":
+/*!*****************************!*\
+  !*** ./src/app/app.pipe.ts ***!
+  \*****************************/
+/*! exports provided: UpperPipe */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "UpperPipe", function() { return UpperPipe; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var UpperPipe = /** @class */ (function () {
+    function UpperPipe() {
+    }
+    UpperPipe.prototype.transform = function (description) {
+        return description[0].toUpperCase() + description.substr(1);
+    };
+    UpperPipe = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Pipe"])({
+            name: 'upper'
+        })
+    ], UpperPipe);
+    return UpperPipe;
 }());
 
 
